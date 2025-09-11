1b2877c2a3c57f414946b1c26c934374f1400a23
RichardSmith240 <smithboy.ricky@gmail.com>
2025-09-11 10:21:41 -0400

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-echo ("This project is not quite ready for installation.")
-echo ("please be patient as we craft a draft build of the program")
+echo "This project is not quite ready for installation."
+echo "Please be patient as we craft a draft build of the program"
 
