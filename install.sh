2ae1757ebbf7c7fbb93d9d2456886d346a0960a8
RichardSmith240 <smithboy.ricky@gmail.com>
2025-09-11 10:26:57 -0400

    Added a curl command for the install.sh script. install.sh is functional via cmd or terminal

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 echo "This project is not quite ready for installation."
-echo "Please be patient as we craft a draft build of the program"
+echo "Please be patient as we draft a working build of this program."
 
