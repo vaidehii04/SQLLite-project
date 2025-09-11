528c0dfecf64634b69b1f860bcdffdb9beecd88f
RichardSmith240 <smithboy.ricky@gmail.com>
2025-09-11 10:31:50 -0400

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -2,11 +2,11 @@ echo "This project is not quite ready for installation."
 echo "Please be patient as we draft a working build of this program."
 
 echo "In the meantime, have a duck."
-echo {'
+echo '
                  ,-.
          ,      ( {o\
          {`"=,___) (`~
           \  ,_.-   )
      ~^~^~^`- ~^ ~^ `~^~^~^~
-'
-}
+	 '
+
