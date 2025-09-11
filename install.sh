580948f7f3de78c82e3984d2372dffc53cc5b390
RichardSmith240 <smithboy.ricky@gmail.com>
2025-09-11 10:30:26 -0400

    install.sh has duck now

--- install.sh
+++ install.sh
@@ -1,3 +1,12 @@
 echo "This project is not quite ready for installation."
 echo "Please be patient as we draft a working build of this program."
 
+echo "In the meantime, have a duck."
+echo {'
+                 ,-.
+         ,      ( {o\
+         {`"=,___) (`~
+          \  ,_.-   )
+     ~^~^~^`- ~^ ~^ `~^~^~^~
+'
+}
