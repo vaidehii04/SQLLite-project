aced7d22d6e3814952da24cf972513f458243d93
RichardSmith240 <smithboy.ricky@gmail.com>
2025-10-09 10:03:29 -0400

    changed the main.java to loginCLI.java for preliminary build (cli stands for command line interface)

---
+++ src/loginCLI.java
@@ -0,0 +1,9 @@
+//dependancies
+import java.util.Scanner;
+
+public loginCLI main{
+	public static void main(String[] args){
+	//Main function to connect all parts of the project. 
+	//The main functionality should all be integrated into here
+	}
+}
