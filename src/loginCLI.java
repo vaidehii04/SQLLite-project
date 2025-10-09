3213b943f60884d3cb4472df8207381d5a7cab27
RichardSmith240 <smithboy.ricky@gmail.com>
2025-10-09 10:42:09 -0400

    started making a login cli

--- src/loginCLI.java
+++ src/loginCLI.java
@@ -3,7 +3,35 @@ import java.util.Scanner;
 
 public loginCLI main{
 	public static void main(String[] args){
-	//Main function to connect all parts of the project. 
-	//The main functionality should all be integrated into here
+		Scanner scnr = new Scanner(System.in);
+	
+		boolean auth;
+
+		//Asking whether the user wants to login with an existing account or create a new account
+		While (auth == False){
+			System.out.print("Would you like to Login or Sign-Up for *xXx* System? [L/s] ");
+			Char loginSign = scnr.next().charAt(0);
+			loginSign = loginSign.toLowerCaes();
+			//checks for option value with login as the default
+			if (loginSign == "" | loginSign == "l" | loginSign == " "){
+				auth = Authenticate();
+			} else if (LoginSign == "s"){
+				SignUp();
+			} else {
+				System.out.print("Please try again");
+			}
+		}
+		scnr.close();
+	}
+
+	//authenication method
+	public boolean Authenticate(){
+		
+	}
+
+	//sign-up method
+	public void SignUp(){
 	}
 }
+
+
