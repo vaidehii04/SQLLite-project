a64c409c7d23fd010ff2b8b57f34312fffe81ada
RichardSmith240 <smithboy.ricky@gmail.com>
2025-10-22 12:36:53 -0400

    edited

--- src/sqlMiddle.java
+++ src/sqlMiddle.java
@@ -12,10 +12,6 @@ public class sqlMiddle {
         {
           statement.setQueryTimeout(30);  // set timeout to 30 sec.
 
-          statement.executeUpdate("drop table if exists person");
-          statement.executeUpdate("create table person (id integer, name string)");
-          statement.executeUpdate("insert into person values(1, 'leo')");
-          statement.executeUpdate("insert into person values(2, 'yui')");
           ResultSet rs = statement.executeQuery("select * from person");
           while(rs.next())
           {
